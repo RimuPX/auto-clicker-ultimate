9156c6a28d899b9953cdd5fa17c31006d6c45b9e
Rick Rimu <shroomappreciator@gmail.com>
2023-04-02 23:22:46 +0300

    Fixed custom widget bug. Updated titlebar

--- UI/Assets/StyleSheets.py
+++ UI/Assets/StyleSheets.py
@@ -1,20 +1,27 @@
-transparentBg = """
-
-"""
-
 titleBar = """
 background-color: rgb(38, 38, 50);
 """
 
-lightBg = """
+titleButton = """
+border-radius: 8;
+"""
+
+lightBox = """
 background-color: rgb(35, 35, 47);
 """
 
-darkBg = """
+darkBox = """
 background-color: rgb(32, 32, 42);
+"""
+
+lightText = """
+color: rgb(74, 74, 103);
+"""
+
+titleText = """
 color: rgb(255, 255, 255);
 """
 
-roundedBox = """
+roundedParam = """
 border-radius: 5;
 """
--- UI/main.py
+++ UI/main.py
@@ -1,46 +1,67 @@
 import sys
 from PyQt6.QtWidgets import *
 from PyQt6.QtCore import Qt, QPoint
-from PyQt6.QtGui import QFont, QFontDatabase
+from PyQt6.QtGui import QFont, QFontDatabase, QPixmap
 from Assets import StyleSheets
 
 app = QApplication(sys.argv)
 
-# Check if title is clicked
-def inBoxBound(position: QPoint, widget: QWidget, centralWidget: QWidget) -> bool:
-    x, y = position.x(), position.y()
-    h, w = widget.geometry().height(), widget.geometry().width()
-    pos = widget.mapTo(centralWidget, QPoint(widget.geometry().x(), widget.geometry().y()))
+class QTitleBar(QLabel):
+    def __init__(self, window: QMainWindow):
+        super(QTitleBar, self).__init__()
+        self.parentWindow = window
+        self.pressOffset = None
 
-    if pos.x() < x < pos.x() + w and pos.y() < y < pos.y() + h:
-        return True
+    def mousePressEvent(self, event) -> None:
+        if event.button() == Qt.MouseButton.LeftButton:
+            self.pressOffset = event.pos()
+        super().mousePressEvent(event)
 
-    return False
+    def mouseMoveEvent(self, event) -> None:
+        if self.pressOffset is not None and event.buttons() == Qt.MouseButton.LeftButton:
+            self.parentWindow.move(self.parentWindow.pos() + event.pos() - self.pressOffset)
+        super().mousePressEvent(event)
 
-class ClickableWidget(QWidget):
-    def __init__(self, window):
-        super().__init__()
+    def mouseReleaseEvent(self, event) -> None:
+        self.pressOffset = None
+        super().mouseReleaseEvent(event)
+
+class QTitleButton(QLabel):
+    def __init__(self, TitleBar: QLabel, pos: int):
+        super(QTitleButton, self).__init__()
+
+        self.setStyleSheet("background-color: red;" + StyleSheets.titleButton)
+        self.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Expanding)
+        buttonHeight = TitleBar.height() - TitleBar.layout().getContentsMargins()[1] - TitleBar.layout().getContentsMargins()[3]
+        self.setFixedWidth(buttonHeight)
+        self.setAlignment(Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignVCenter)
+        TitleBar.layout().addWidget(self, pos)
+
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(QMainWindow, self).__init__()
 
+        # Adds font to database
+        QFontDatabase.addApplicationFont("Assets/Fonts/Sublima-ExtraBold.otf")
+        self.PropertyFont = QFont("Sublima ExtraBold", 18)
+
         # Defines starting size and minimum size for main window
         divFactor = 3
         divFactor = int(round(divFactor))
-        screenSize = app.primaryScreen().geometry()
-        self.resize(screenSize.width() // divFactor, screenSize.height() // divFactor)
-        self.setMinimumSize(screenSize.width() // divFactor, screenSize.height() // divFactor)
+        self.screenSize = app.primaryScreen().geometry()
+        self.resize(self.screenSize.width() // divFactor, self.screenSize.height() // divFactor)
+        self.setMinimumSize(self.screenSize.width() // divFactor, self.screenSize.height() // divFactor)
 
         # Creates central widget
         self.CentralWidget = QWidget()
-        self.CentralWidget.setStyleSheet(StyleSheets.darkBg)
+        self.CentralWidget.setStyleSheet(StyleSheets.darkBox)
         self.CentralWidget.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
         self.setCentralWidget(self.CentralWidget)
 
         # Set window attributes
         self.setWindowFlag(Qt.WindowType.FramelessWindowHint)
-        # self.setWindowFlag(Qt.WindowType.WindowStaysOnTopHint)
         self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground, True)
 
         # Sets up main window's layout
@@ -50,31 +71,41 @@ class MainWindow(QMainWindow):
         self.MainLayout.setSpacing(0)
         self.MainLayout.setSizeConstraint(QLayout.SizeConstraint.SetDefaultConstraint)
 
-
-
-
-        self.p = ClickableWidget(self)
-        self.p.setVisible(True)
-        self.p.show()
-        self.p.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
-        self.p.setStyleSheet("background-color: rgb(0, 0, 255);")
-        self.MainLayout.addWidget(self.p)
-
-
-
-
         # Creates custom title bar
-        self.TitleBar = QWidget(self.CentralWidget)
+        self.TitleEdgeOffset = 4
+
+        self.TitleBar = QTitleBar(self)
         self.TitleBar.setStyleSheet(StyleSheets.titleBar)
         self.TitleBar.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed)
-        self.TitleBar.setFixedHeight(screenSize.height() // 50)
+        self.TitleBar.setFixedHeight(self.screenSize.height() // 50)
         self.TitleBar.setLayout(QHBoxLayout())
-
+        self.TitleBar.layout().setContentsMargins(self.TitleEdgeOffset, self.TitleEdgeOffset, self.TitleEdgeOffset, self.TitleEdgeOffset)
+        self.TitleBar.layout().setSpacing(self.TitleEdgeOffset)
         self.MainLayout.addWidget(self.TitleBar, 0, 0, 1, 2)
+
+        # Creates title and icon
+        self.Title = QLabel()
+        self.Logo = QPixmap("Assets/Icons/Logo.png")
+        self.Logo = self.Logo.scaledToHeight(self.TitleBar.height() - self.TitleEdgeOffset * 2)
+        self.Title.setFixedSize(self.TitleBar.height() - self.TitleEdgeOffset * 2, self.TitleBar.height() - self.TitleEdgeOffset * 2)
+        self.Title.setPixmap(self.Logo)
+        self.TitleBar.layout().addWidget(self.Title, 0)
+
+        self.Title = QLabel("Macro Ultimate")
+        self.Title.setFont(QFont("Sublima ExtraBold", 10))
+        self.Title.setStyleSheet(StyleSheets.lightText)
+        self.TitleBar.layout().addWidget(self.Title, 0)
+
+        self.TitleBar.layout().insertSpacing(2, self.screenSize.width())
+
+        # Creates custom titlebar buttons
+        self.ExitButton = QTitleButton(self.TitleBar, 0)
+        self.MinimizeButton = QTitleButton(self.TitleBar, 0)
+
 
         # Sets up container for command nodes
         self.NodeContainer = QWidget(self.CentralWidget)
-        self.NodeContainer.setStyleSheet(StyleSheets.lightBg)
+        self.NodeContainer.setStyleSheet(StyleSheets.lightBox)
         self.NodeContainer.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
         self.NodeContainer.setFixedWidth(self.width() // 3)
         self.NodeContainer.setLayout(QVBoxLayout())
@@ -83,20 +114,19 @@ class MainWindow(QMainWindow):
 
         # Sets up container for command nodes' properties
         self.PropertyContainer = QWidget(self.CentralWidget)
-        self.PropertyContainer.setStyleSheet(StyleSheets.darkBg)
+        self.PropertyContainer.setStyleSheet(StyleSheets.darkBox)
         self.PropertyContainer.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
         self.PropertyContainer.setLayout(QVBoxLayout())
         self.PropertyContainer.layout().setContentsMargins(10, 0, 10, 10)
 
-        QFontDatabase.addApplicationFont("Assets/Fonts/Sublima-ExtraBold.otf")
-        self.PropertyFont = QFont("Sublima ExtraBold", 18)
+        # Sets up property section elements
         self.PropertyTitle = QLabel("Properties")
         self.PropertyTitle.setFont(self.PropertyFont)
-        self.PropertyTitle.setStyleSheet(StyleSheets.darkBg)
+        self.PropertyTitle.setStyleSheet(StyleSheets.titleText)
         self.PropertyContainer.layout().addWidget(self.PropertyTitle)
 
         self.PropertyBox = QWidget(self.CentralWidget)
-        self.PropertyBox.setStyleSheet(StyleSheets.lightBg + StyleSheets.roundedBox)
+        self.PropertyBox.setStyleSheet(StyleSheets.lightBox + StyleSheets.roundedParam)
         self.PropertyBox.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
         self.PropertyBox.setLayout(QVBoxLayout())
         self.PropertyContainer.layout().addWidget(self.PropertyBox, 1)
@@ -109,26 +139,11 @@ class MainWindow(QMainWindow):
         self.CornerGripWidget = QSizeGrip(self.CentralWidget)
         self.CornerGripWidget.setStyleSheet("background-color: rgb(255, 0, 0);")
 
-        # Declares window dragging variables
-        self.pressOffset = None
-
     def resizeEvent(self, event) -> None:
         # Reposition scaling corner
         self.CornerGripWidget.setGeometry(self.width() - self.gripBorderOffset, self.height() - self.gripBorderOffset, self.gripBorderOffset, self.gripBorderOffset)
 
-    def mousePressEvent(self, event) -> None:
-        if event.button() == Qt.MouseButton.LeftButton and inBoxBound(event.pos(), self.TitleBar, self.centralWidget()):
-            self.pressOffset = event.pos()
-        super().mousePressEvent(event)
 
-    def mouseMoveEvent(self, event) -> None:
-        if self.pressOffset is not None and event.buttons() == Qt.MouseButton.LeftButton:
-            self.move(self.pos() + event.pos() - self.pressOffset)
-        super().mousePressEvent(event)
-
-    def mouseReleaseEvent(self, event) -> None:
-        self.pressOffset = None
-        super().mouseReleaseEvent(event)
 
 def main():
     window = MainWindow()
